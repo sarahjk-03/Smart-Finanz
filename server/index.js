8df985b8270590a7d05f76c4bbcc0443a72c4b19
Sarah <sarah4jk@gmail.com>
2026-03-17 16:01:15 +0530

    fix cors crash

--- server/index.js
+++ server/index.js
@@ -22,17 +22,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like Postman)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  origin: "*"
 }));
 
 app.use(express.json());
